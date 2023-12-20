42263d119932134d9d979b1e0f72ba27909f9504
Jakub Zarzycki <jakub.zarzycki@ideas-ncbr.pl>
2023-12-20 13:07:07 +0100

    fix

--- NGQN/network.py
+++ NGQN/network.py
@@ -15,30 +15,34 @@ class GCN(nn.Module):
         self.n = number_of_actions
 
         state, target = observation
+        self.in_size = state * (state // 4)
 
         self.conv_model = nn.Sequential(nn.Linear(4, 64),
                                    nn.ReLU(),
                                    nn.Linear(64, observation[0]),
                                    )
 
-        self.conv1 = EdgeConv(self.conv_model, aggr="sum")
+        self.conv1 = EdgeConv(self.conv_model, aggr="mean")
 
-        self.model = nn.Sequential(nn.Linear(observation[0], 128),
+        self.model = nn.Sequential(nn.Linear(self.in_size, 256),
                                    nn.LeakyReLU(),
-                                   nn.Linear(128, 64),
+                                   nn.Linear(256, 256),
+                                   nn.LeakyReLU(),
+                                   nn.Linear(256, 128),
                                    nn.LeakyReLU(),
                                    )
 
         self.pooling = nn.AvgPool1d(4)
 
-        self.value_head = nn.Linear(64, 1)
+        self.value_head = nn.Linear(128, 1)
 
-        self.adv_heads = nn.ModuleList([nn.Linear(64, action_space_dimension) for _ in range(number_of_actions)])
+        self.adv_heads = nn.ModuleList([nn.Linear(128, action_space_dimension) for _ in range(number_of_actions)])
 
     def forward(self, x: torch.Tensor, edge_index: torch.Tensor):
         out = self.conv1(x, edge_index)
 
-        out = self.pooling(out).relu().t()
+        out = self.pooling(out)
+        out = out.reshape((1, self.in_size))
         out = self.model(out)
         value = self.value_head(out)
         advantages = torch.stack([l(out) for l in self.adv_heads], dim=1)
